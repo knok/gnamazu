f7fd21add94de2604f3ad9088e3af05279616413
NOKUBI Takatsugu <knok@daionet.gr.jp>
2012-07-22 01:08:34 +0900

    Create Namazu-like index

---
+++ mkdb.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+
+DBNAME=Namazu/NMZ.db
+
+test -f $DBNAME && rm -f NMZ.db*
+
+groonga -n $DBNAME < /dev/null
+
+groonga $DBNAME table_create --name Fields --flags TABLE_HASH_KEY \
+    --key_type ShortText
+groonga $DBNAME table_create --name Index --flags TABLE_PAT_KEY\|KEY_NORMALIZE \
+    --key_type ShortText --default_tokenizer TokenBigram
+
+groonga $DBNAME column_create --table Fields --name date --flags COLUMN_SCALAR --type Time
+groonga $DBNAME column_create --table Fields --name from --flags COLUMN_SCALAR --type ShortText
+groonga $DBNAME column_create --table Fields --name message_id --flags COLUMN_SCALAR --type ShortText
+groonga $DBNAME column_create --table Fields --name newsgroups --flags COLUMN_SCALAR --type ShortText
+groonga $DBNAME column_create --table Fields --name subject --flags COLUMN_SCALAR --type ShortText
+groonga $DBNAME column_create --table Fields --name to --flags COLUMN_SCALAR --type ShortText
+groonga $DBNAME column_create --table Fields --name url --flags COLUMN_SCALAR --type ShortText
+groonga $DBNAME column_create --table Fields --name body --flags COLUMN_SCALAR --type Text
+
+groonga $DBNAME column_create --table Index --name body_index \
+    --flags COLUMN_INDEX\|WITH_SECTION\|WITH_POSITION --type Fields \
+    --source body,from,message_id,subject,to,url
