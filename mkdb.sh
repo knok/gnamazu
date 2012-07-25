86c81bcb6ff4816d3d2d90fb4c72b021558f8d9c
NOKUBI Takatsugu <knok@daionet.gr.jp>
2012-07-26 08:49:23 +0900

    Add version info

--- mkdb.sh
+++ mkdb.sh
@@ -4,6 +4,7 @@
 DBNAME=Namazu/NMZ.db
 
 test -f $DBNAME && rm -f Namazu/NMZ.db*
+mkdir Namazu
 
 groonga -n $DBNAME < /dev/null
 
@@ -11,6 +12,8 @@ groonga $DBNAME table_create --name Fields --flags TABLE_HASH_KEY \
     --key_type ShortText
 groonga $DBNAME table_create --name Index --flags TABLE_PAT_KEY\|KEY_NORMALIZE \
     --key_type ShortText --default_tokenizer TokenBigram
+groonga $DBNAME table_create --name Version --flags TABLE_HASH_KEY \
+    --key_type ShortText
 
 groonga $DBNAME column_create --table Fields --name date --flags COLUMN_SCALAR --type Time
 groonga $DBNAME column_create --table Fields --name from --flags COLUMN_SCALAR --type ShortText
@@ -25,3 +28,13 @@ groonga $DBNAME column_create --table Index --name body \
     --flags COLUMN_INDEX\|WITH_SECTION\|WITH_POSITION --type Fields \
     --source body
 #    --source body,from,message_id,subject,to,url
+
+groonga $DBNAME column_create --table Version --name number --flags COLUMN_SCALAR --type ShortText
+groonga $DBNAME <<EOF
+load --table Version 
+[ 
+	{'_key': "version", 
+	"number": "1.0" 
+	} 
+] 
+EOF
